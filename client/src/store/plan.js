51f53893ca714c41954a5733dc8dd629e3649462
Martin <cauu@163.com>
2018-06-19 01:05:56 +0800

    init store

--- client/src/store/plan.js
+++ client/src/store/plan.js
@@ -1,1 +1,11 @@
 import {observable, computed, reaction} from 'mobx';
+
+/**
+ * @todo 
+ * 1. 搜索所有plan
+ * 2. 根据关键字（tag，dest）获取对应行程
+ * 3. 获取推荐行程
+ * 4. 当前行程detail
+ */
+export default class PlanStore {
+}
